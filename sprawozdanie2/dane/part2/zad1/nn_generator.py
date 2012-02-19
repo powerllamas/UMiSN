e696af07d822927b39f7da2a5d47160930466508
Tomek <tomek.zietkiewicz@gmail.com>
2012-02-19 17:07:07 +0100

    PArt 2 task 1 data

--- sprawozdanie2/dane/part2/zad1/nn_generator.py
+++ sprawozdanie2/dane/part2/zad1/nn_generator.py
@@ -5,28 +5,32 @@ Simple NN generator - given number of neurons in each layer it generates feed-fo
 First layer is input layer: all neurons in this layer have no inputs, therefore number of neurons in the first layer is equal to number of inputs of the network.
 '''
 def generate(neuron_type, layers):        
-    print "\nX",
+    print"""org:
+name: Anonymous framstick
+genotype:~"""
+    genotype = "X"
     inputs_no = 0  
     for layer, layer_size in enumerate(layers):
         if(layer > 0):
             inputs_no = layers[layer - 1]
         first_input = 1
         for neuron in range(layer_size):
-            print "[%s" % (neuron_type, ),
+            genotype += "[%s" % (neuron_type, )
             for input_neuron in range(first_input, first_input + inputs_no):
                 weight = random()
-                print ", %d:%f" % (-input_neuron, weight),
-            print "]",
+                genotype += ", %d:%f" % (-input_neuron, weight)
+            genotype += "]"
             first_input +=  1
+    print genotype
+    print "~\n"
             
 
 if __name__ == '__main__':
     if sys.argv[1] == '-h' or sys.argv[1] == '--help':
         print "usage: nn_generator.py N a b c\nwhere:\n\tN - number of genotypes to generate\n\ta - number of neurons in first layer\n\tb-number of neurons in 2 layer\n\tc...\nExample:\n\tnn_generator.py 5 3 4 1\nwill generate 5 genotypes, each with neural network of architecture 3-4-3."
     else:
-        N = sys.argv[1]
+        N = int(sys.argv[1])
         layers = [int(c) for c in sys.argv[2:]]
-        print layers
         neuron_type = "Nu"
-        for i in range(0,int(N)):
+        for i in range(0,N):
             generate(neuron_type, layers)
