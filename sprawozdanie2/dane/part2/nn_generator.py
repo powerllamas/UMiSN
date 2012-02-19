b51921c6bcb1306e14144bf4df3a105cbf8ea24f
Tomek <tomek.zietkiewicz@gmail.com>
2012-02-19 13:04:43 +0100

    Fixewd and improved generator

--- sprawozdanie2/dane/part2/nn_generator.py
+++ sprawozdanie2/dane/part2/nn_generator.py
@@ -1,26 +1,28 @@
 from random import random
-
+import sys
 '''
 Simple NN generator - given number of neurons in each layer it generates feed-forward network in f1 notation, with random weights.
-First layer is input layer: ale neurons in this layer have no inputs, therefore number of neurons in the first layer is equal to number of inputs of the network.
+First layer is input layer: all neurons in this layer have no inputs, therefore number of neurons in the first layer is equal to number of inputs of the network.
 '''
 def generate(neuron_type, layers):        
-    print "X"
-    inputs_no = 0
-    first_input = 0
+    print "\nX",
+    inputs_no = 0  
     for layer, layer_size in enumerate(layers):
         if(layer > 0):
             inputs_no = layers[layer - 1]
+        first_input = 1
         for neuron in range(layer_size):
             print "[%s" % (neuron_type, ),
             for input_neuron in range(first_input, first_input + inputs_no):
                 weight = random()
-                print ", %d:%f" % (input_neuron, weight),
-            print "]"
-        first_input +=  inputs_no
+                print ", %d:%f" % (-input_neuron, weight),
+            print "]",
+            first_input +=  1
             
 
 if __name__ == '__main__':
-    layers = [3, 4, 3]
-    neuron_type = "N"
-    generate(neuron_type, layers)
+    layers = [int(c) for c in sys.argv[1:]]
+    print layers
+    neuron_type = "Nu"
+    for i in range(0,2):
+        generate(neuron_type, layers)
